402b71a120da46d3ff7282c7931bc36bebb9da76
Donsky1 <falson2007@yandex.ru>
2022-03-21 04:45:47 +0300

    update lessons about function theme

--- borndayforewer.py
+++ borndayforewer.py
@@ -6,13 +6,40 @@
 Задание: переписать код используя как минимум 1 функцию
 """
 
-year = input('Ввведите год рождения А.С.Пушкина:')
-while year != '1799':
-    print("Не верно")
-    year = input('Ввведите год рождения А.С.Пушкина:')
+# year = input('Ввведите год рождения А.С.Пушкина:')
+# while year != '1799':
+#     print("Не верно")
+#     year = input('Ввведите год рождения А.С.Пушкина:')
+#
+# day = input('Ввведите день рождения Пушкин?')
+# while day != '6':
+#     print("Не верно")
+#     day = input('В какой день июня родился Пушкин?')
+# print('Верно')
 
-day = input('Ввведите день рождения Пушкин?')
-while day != '6':
-    print("Не верно")
-    day = input('В какой день июня родился Пушкин?')
-print('Верно')
+def check_year(year):
+    while year != '1799':
+        print("Неверно")
+        year = input('Ввведите год рождения А.С.Пушкина: ')
+
+    return True
+
+def check_day(day):
+    while day != '6':
+        print("Неверно")
+        day = input('В какой день июня родился Пушкин? ')
+
+    return True
+
+def check_birthday_pushkin():
+    year = input('Ввведите год рождения А.С.Пушкина: ')
+    true_year = check_year(year)
+
+    day = input('Ввведите день рождения Пушкина? ')
+    true_day = check_day(day)
+
+    if true_year and true_day:
+        print('Верно')
+
+
+check_birthday_pushkin()
--- use_functions.py
+++ use_functions.py
@@ -33,21 +33,55 @@
 
 Для реализации основного меню можно использовать пример ниже или написать свой
 """
+from datetime import datetime
+
+total_cash = 0.0
+history = {}
+
+def top_up_on_account(total):
+    print('Вы только что внесли сумму', total)
+    return total
 
 while True:
+    print()
     print('1. пополнение счета')
     print('2. покупка')
     print('3. история покупок')
-    print('4. выход')
+    print('4. запросить баланс')
+    print('5. выход')
+    print()
 
-    choice = input('Выберите пункт меню')
+    choice = input('Выберите пункт меню: ')
+    # пополнение счета
     if choice == '1':
-        pass
+        total_cash += float(input(f'На текущий момент на Вашем счете {total_cash} усл.ед. \nНа какую сумму Вы хотите пополнить счет: '))
+        continue
+    # покупка
     elif choice == '2':
-        pass
+        price_bag = float(input('Введите сумму покупки: '))
+        if price_bag > total_cash:
+            print('К сожалению на Вашем счете недостаточно денег: ')
+            print('-' * 30)
+        else:
+            bag = input('Название покупки: ')
+            # В историю записывается инф в  виде словаря {время: название покупки и цена}
+            history[str(datetime.now().time())] = (bag, price_bag)
+            total_cash -= price_bag
+    # история покупок
     elif choice == '3':
-        pass
+        if history == {}:
+            print('Вы еще не совершали покупок')
+        else:
+          print()
+          print('{} {:>15} {}'.format('Дата покупки', 'Товар', 'Стоимость'))
+          for k, v in history.items():
+            print(f'{k} {v}')
+    # запросить баланс
     elif choice == '4':
-        break
+        print(f'Баланс: {total_cash}')
+    # выход
+    elif choice == '5':
+        exit()
+        # break
     else:
         print('Неверный пункт меню')
